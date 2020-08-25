aac6c98dbb19b24c2f6d27cb11668f2031deccc6
jsandersii <7421274+jsandersii@users.noreply.github.com>
2020-08-25 17:59:52 -0500

    Adding Android Project

--- app/src/main/java/com/example/codingclass/MainActivity.java
+++ app/src/main/java/com/example/codingclass/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String checkQuestion5 = CoreLogic.withoutString("Hello there llo", "llo");
+        String checkQuestion5 = CoreLogic.withoutString("Hello there", "llo");
         System.out.print(checkQuestion5);
     }
 }
